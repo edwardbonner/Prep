6ac62d999b5887dcfba92b5e9a02920988ed618e
edwardbonner <41027884+edwardbonner@users.noreply.github.com>
2020-07-27 09:02:06 -0400

    Add files via upload

---
+++ Prospects.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PrepAndroid
+{
+    public class Prospects
+    {
+        public int ID { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public Nullable<DateTime> ContactDate { get; set; }
+
+
+    }
+}
